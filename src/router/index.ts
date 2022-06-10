703355a29263a70db440965f0c0c609a1c227eb2
“huangxin <huang201534110@163.com>
2022-06-10 09:45:38 +0800

    add line3dTile

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
 import { Router, createRouter, createWebHashHistory } from "vue-router";
+import index from "@/view/index/index.vue"
 import cityMain from "@/view/main-city/city-main.vue"
+import census from "@/view/census/index.vue"
 const routes = [
-    { path: '/', component: cityMain },
+    { name: '首页', path: '/', component: index },
+    { name: '城市', path: '/city', component: cityMain },
+    { name: '统计', path: '/census', component: census },
 ]
 export const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
---
+++ src/view/index/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <vertical></vertical>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, toRefs } from "vue";
+import vertical from "../layout/sidebar/vertical.vue";
+</script>
+<style scoped lang="scss"></style>
